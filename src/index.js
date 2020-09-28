7112c9d4e814d5f1286e9bda31765a09a1d3b44e
Oleh Leochko <olegleochko7@gmail.com>
2020-09-28 23:47:05 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-
-exports.min = function min (array) {
-  return 0;
+function bubbleSort(arr) {
+    let tmp = 0;
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
+    return arr;
 }
+exports.min = function min(array) {
+    if (typeof array === "undefined" || array.length === 0) {
+        return 0;
+    }
+    let arrSort = bubbleSort(array);
+    return arrSort[0];
+    //return array.sort((a, b) => a - b)[0];
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (typeof array === "undefined" || array.length === 0) {
+        return 0;
+    }
+    let arrSort = bubbleSort(array);
+    return arrSort[array.length - 1];
+    //return array.sort((a, b) => a - b)[array.length - 1];
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (typeof array === "undefined" || array.length === 0) {
+        return 0;
+    }
+    return array.reduce((a, b) => a + b) / array.length;
+};
